a9f2911100bf6e2857ede50b2525c0b42d17283f
Igor Kukric <igorkukric@gmail.com>
2023-09-22 23:59:52 +0200

    add useEffect for moviDetails add isloading

--- src/pages/movies/MovieDetails.jsx
+++ src/pages/movies/MovieDetails.jsx
@@ -1,12 +1,41 @@
-import { Box, Heading } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { getDetails } from "../../services/api";
+import { useParams } from "react-router-dom";
 
 const MovieDetails = () => {
-  return (
-    <Box mt='6'>
-        <Heading>Movie Details</Heading>
-    </Box>
-  )
-}
+  const router = useParams();
+  const [details, setDetails] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-export default MovieDetails
+  const { id } = router;
+
+  useEffect(() => {
+    getDetails("movie", id)
+      .then((res) => {
+        console.log(res, "res");
+        setDetails(res);
+      })
+      .catch((err) => {
+        console.log(err, "err");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <Box mt="6">
+      {isLoading ? (
+        <Box textAlign="center">
+          <Spinner size="lg" />
+        </Box>
+      ) : (
+        <Box>
+          <Heading>{details?.title} </Heading>
+          <Text color="gray.400" size="xs">{details?.original_title} </Text>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default MovieDetails;
