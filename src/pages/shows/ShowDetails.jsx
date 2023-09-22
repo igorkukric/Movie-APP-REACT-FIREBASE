777314aa5d584e37b38f2db0232bd55512d3e5ec
Igor Kukric <igorkukric@gmail.com>
2023-09-23 00:21:12 +0200

    fix ShowDetails

--- src/pages/shows/ShowDetails.jsx
+++ src/pages/shows/ShowDetails.jsx
@@ -1,12 +1,42 @@
-import { Box, Heading } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { getDetails } from "../../services/api";
+import { useParams } from "react-router-dom";
 
 const ShowDetails = () => {
-  return (
-    <Box mt='6'>
-        <Heading>Show Details</Heading>
-    </Box>
-  )
-}
+  const router = useParams();
+  const [details, setDetails] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-export default ShowDetails
+  const { id } = router;
+
+  useEffect(() => {
+    getDetails("tv", id)
+      .then((res) => {
+        console.log(res, "res");
+        setDetails(res);
+      })
+      .catch((err) => {
+        console.log(err, "err");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+  return (
+    <Box mt="6">
+      {isLoading ? (
+        <Box textAlign="center">
+          <Spinner size="lg" />
+        </Box>
+      ) : (
+        <Box>
+          <Heading>{details?.name} </Heading>
+          <Text color="gray.400" size="xs">
+            {details?.original_name}{" "}
+          </Text>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default ShowDetails;
