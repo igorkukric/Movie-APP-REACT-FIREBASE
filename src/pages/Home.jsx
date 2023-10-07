ea01d21b33ccf38b4568615227975cfda4c1f4f1
Igor_Kukric <igorkukric@gmail.com>
2023-10-07 16:49:36 +0200

    import observer and get more trending

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Box, Grid, Heading, Skeleton } from "@chakra-ui/react";
 import { getTrending } from "../services/api";
 import CardComponent from "../components/CardComponent";
@@ -6,13 +6,18 @@ import CardComponent from "../components/CardComponent";
 const Home = () => {
   const [media, setMedia] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);  // Current page number
+  const loaderRef = useRef(null);  // Ref for the loader element
+
+  const loadMoreData = () => {
+    setPage(page + 1);
+  };
 
   useEffect(() => {
     setIsLoading(true);
-    getTrending()
+    getTrending(page)  // Fetch trending items for the current page
       .then((res) => {
-        // console.log(res, 'media')
-        setMedia(res?.results);
+        setMedia((prevMedia) => [...prevMedia, ...res?.results]);  // Append new items to the existing list
       })
       .catch((err) => {
         console.log(err, "error");
@@ -20,20 +25,47 @@ const Home = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, [page]);
 
-  console.log(media, "media");
+  // Use IntersectionObserver to load more data when user scrolls to the loader element
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        loadMoreData();
+      }
+    }, options);
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
+  }, []);
 
   return (
     <Box mt="6">
       <Heading fontSize="2xl">Trending now</Heading>
-      <Grid templateColumns={{
-            base: "1fr",
-            md: "repeat(3, 1fr)",
-            lg: "repeat(5, 1fr)",
-          }} gap={6} mt="6">
-        {media?.map((item) =>
-          isLoading ? (
+      <Grid
+        templateColumns={{
+          base: "1fr",
+          md: "repeat(3, 1fr)",
+          lg: "repeat(5, 1fr)",
+        }}
+        gap={6}
+        mt="6"
+      >
+        {media?.map((item, index) =>
+          isLoading && index === media.length - 1 ? (
             <Skeleton
               key={item?.id}
               borderRadius={"lg"}
@@ -45,8 +77,10 @@ const Home = () => {
           )
         )}
       </Grid>
+      <div ref={loaderRef} style={{ height: "20px" }}></div>
     </Box>
   );
 };
 
 export default Home;
+
